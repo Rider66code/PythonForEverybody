41f90f8e6dfe2febb63b731c7d3042bb7d8a32a4
Rider66code <59551376+Rider66code@users.noreply.github.com>
2020-01-06 08:59:42 +0300

    Create ex_04_06.py

---
+++ ex_04_06/ex_04_06.py
@@ -0,0 +1,34 @@
+# 2.3 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking or bad user data.
+# 3.1 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking the user input - assume the user types numbers properly.
+# 4.6 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Pay should be the normal rate for hours up to 40 and time-and-a-half for the hourly rate for all hours worked above 40 hours. Put the logic to do the computation of pay in a function called computepay() and use the function to do the computation. The function should return a value. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking the user input unless you want to - you can assume the user types numbers properly. Do not name your variable sum or use the sum() function.
+# This first line is provided for you
+
+def computepay():
+    if fhrs>40:
+        pay = 40*frate+(fhrs-40)*(frate*1.5)
+
+    else:
+        #pay = fhrs*frate
+        pay=str('Lol, notting')
+
+    return pay
+
+
+hrs = input("Enter Hours:\n")
+try:
+    fhrs=float(hrs)
+
+except:
+    print("Can't parse hours value, closing.")
+    quit()
+
+rate = input("Enter Rate:\n")
+try:
+    frate=float(rate)
+
+except:
+    print ("Can't parse rate value, closing.")
+    quit()
+
+p = computepay()
+print(p)
